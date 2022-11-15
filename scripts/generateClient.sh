caa2fddfb42e651c9ddb8838d7a5f0114cf4d705
Henry Recker <henryrecker@pingidentity.com>
2022-11-15 15:28:22 -0600

    Update generation logic to delete unused structs

--- scripts/generateClient.sh
+++ scripts/generateClient.sh
@@ -2,6 +2,11 @@
 
 set -x
 
+# First remove any existing files that were generated. If we want to edit files post-generation, this section will need to be removed
+rm ./api_*.go
+rm ./model_*.go
+rm -r docs/
+
 openapi-generator generate -i complete-api-spec/openapi.yaml -g go --git-host github.com --git-repo-id pingdata-config-api-go-client --git-user-id pingidentity
 
 rm -r test/
