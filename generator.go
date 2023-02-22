f68ea26c4a5254170cb6acd56701cec4995dfb37
henryrecker-pingidentity <58565376+henryrecker-pingidentity@users.noreply.github.com>
2023-02-22 15:54:45 -0600

    Move client go files to subfolder package (#10)
    * Move go files to configurationapi package
    
    * Update code check to build subdirectories
    
    * Remove old files
    
    * Add README

--- generator.go
+++
@@ -1,6 +0,0 @@
-package openapi
-
-// This file contains any go generate comments used by the client
-
-//go:generate ./scripts/generateEnumConversionFunctions.py
-//go:generate ./scripts/setUserPrivilegeEnumNames.py
